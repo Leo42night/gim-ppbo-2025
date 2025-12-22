d9a3df62e25c616870a96f88b3ec110126d4acfd
Leo42night <karmaborutovvo@gmail.com>
2025-12-22 15:42:13 +0700

    new config

--- App/Core/Database.php
+++ App/Core/Database.php
@@ -1,70 +1,88 @@
 <?php
 
-class Database {
-  private $host = DB_HOST;
-  private $port = DB_PORT;
+class Database
+{
+  private $instance = DB_INSTANCE; // project:region:instance
   private $name = DB_NAME;
   private $user = DB_USER;
-  private $pass = DB_PASS;  
-  private $dbh; // database handler
+  private $pass = DB_PASSWORD;
+  private $dbh;
   private $stmt;
-  
-  public function __construct() {
-    // data source name
-   $dsn = 'mysql:host='.$this->host.':'.$this->port.';dbname='.$this->name;
-    $option=[
+
+  public function __construct()
+  {
+    $dsn = sprintf(
+      'mysql:unix_socket=/cloudsql/%s;dbname=%s;charset=utf8mb4',
+      $this->instance,
+      $this->name
+    );
+
+    $options = [
       PDO::ATTR_PERSISTENT => true,
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Make the default fetch be an associative array
-      ];
-    
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
     try {
-      $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-    } catch(Exception $e) {
-      error_log($e->getMessage());
-      exit('Something bad happened');
+      if (getenv('K_SERVICE')) {
+        // Cloud Run
+        $dsn = "mysql:unix_socket=/cloudsql/{$this->instance};dbname={$this->name}";
+      } else {
+        // Local
+        $dsn = "mysql:host=localhost;port=3306;dbname={$this->name}";
+      }
+
+      $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+    } catch (PDOException $e) {
+      die('DB Connection failed: ' . $e->getMessage());
     }
   }
-  
-  public function query($query) {
+
+  public function query($query)
+  {
     $this->stmt = $this->dbh->prepare($query);
   }
-  
-  public function bind($param, $value, $type=null){
-    if(is_null($type)) {
-      switch(true){
-        case is_int($value) :
+
+  public function bind($param, $value, $type = null)
+  {
+    if (is_null($type)) {
+      switch (true) {
+        case is_int($value):
           $type = PDO::PARAM_INT;
           break;
-        case is_bool($value) :
+        case is_bool($value):
           $type = PDO::PARAM_BOOL;
           break;
-        case is_null($value) :
+        case is_null($value):
           $type = PDO::PARAM_NULL;
           break;
         default:
           $type = PDO::PARAM_STR;
       }
     }
-    
+
     $this->stmt->bindValue($param, $value, $type);
   }
-  
-  public function execute(){
+
+  public function execute()
+  {
     $this->stmt->execute();
   }
-  
-  public function resultSet(){
+
+  public function resultSet()
+  {
     $this->execute();
     return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
-  
-  public function single(){
+
+  public function single()
+  {
     $this->execute();
     return $this->stmt->fetch(PDO::FETCH_ASSOC);
   }
 
-  public function rowCount() {
+  public function rowCount()
+  {
     return $this->stmt->rowCount();
   }
 }
--- App/config/config.php
+++ App/config/config.php
@@ -13,8 +13,6 @@ $base_url = strpos($host, 'localhost') !== false ?
 // ?? will skip error
 // ?: will show if key doesn't exist
 define('BASE_URL', $base_url);
-define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
-define('DB_PORT', $_ENV['DB_PORT'] ?? '3306');
 define('DB_NAME', $_ENV['DB_NAME'] ?? 'kelas_pbo');
 define('DB_USER', $_ENV['DB_USER'] ?? 'root');
 define('DB_PASS', $_ENV['DB_PASS'] ?? '');
